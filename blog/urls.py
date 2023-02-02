ae193b20ffc24ebdf9e9e1792c90fe45b6d6c970
Bastian8004 <paszko08042004@gmail.com>
2023-02-02 11:00:49 +0100

    zmiany - działa - obrazek???

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.post_list,name="post_list"),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('', views.Image.as_view(), name='image'),
-    path('', views.ImageDisplay.as_view(), name='image_display'),
+    path('image/', views.Image.as_view(), name='image'),
+    path('image/<int:pk>/', views.ImageDisplay.as_view(), name='image_display'),
     #path('', views.index, name='index'), - zawsze kiedy dodajemy nowe widoki
 ]
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,12 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     #path(r'^admin/(\d+)$',admin.site.urls)
     path("",include('blog.urls')),
+
 ]
 
 if settings.DEBUG:
